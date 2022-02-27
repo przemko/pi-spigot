1f534f3d283502feaa71b94d3e20d53394dc2f18
Przemysław Kobylański <przemko@mac.com>
2022-02-27 18:24:42 +0100

    program in Python language

---
+++ python/pi_spigot.py
@@ -0,0 +1,10 @@
+def g(q, r, t, i):
+    while(True):
+        u = 3 * (3 * i + 1) * (3 * i + 2)
+        y = (q * (27 * i - 12) + 5 * r) // (5 * t)
+        yield y
+        q, r, t, i = 10*q*i*(2*i-1), 10*u*(q*(5*i-2)+r-y*t), t*u, i+1
+
+for x in g(1, 180, 60, 2):
+    print(x,end='')
+
